e44cb83667e5e92bd588a5d3e79830dda2b30dc7
Vatsal Agarwal <vatsal.smile@gmail.com>
2016-08-29 21:31:26 +0530

    Create Constructor.java

---
+++ Constructor.java
@@ -0,0 +1,17 @@
+//Initialisation of variables through Constructor//
+public class Cons2 {
+	int a;
+	String str;
+	double d;
+	
+	Cons2()
+	{
+	System.out.println("A is "+a);	
+	System.out.println("Str is "+str);
+	System.out.println("d is "+d);
+	}
+ public static void main(String []ar)
+ {
+	 Cons2 d=new Cons2();
+ }
+}
